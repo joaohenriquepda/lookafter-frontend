26b6eccaf45809da6f7de971ef9a38b54f695f39
João Henrique <joaohenrique.p.almeida@gmail.com>
2020-05-13 04:11:07 -0300

    Adding services for auth user

---
+++ src/app/services/auth-user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthUserService {
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post(environment.serverUrl + '/users', user, httpOptions);
+  }
+
+  authUser( data) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post(environment.serverUrl + '/auth/login', data, httpOptions);
+  }
+}
