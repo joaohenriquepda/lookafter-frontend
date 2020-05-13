9288e7ae00ee839c5c96adc6e29e1d577ec9ec15
João Henrique <joaohenrique.p.almeida@gmail.com>
2020-05-13 01:39:40 -0300

    Adding landing module page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'landing',
+    loadChildren: () =>
+      import("./landing-page/landing-page/landing-page.module").then((m) => m.LandingPageModule),
+    canActivate: [],
+  },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '404', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
